5ed52d504a042a20e3e453b5d3ee5e3360eeebaf
qkransrb <qkransrb90@gmail.com>
2024-12-20 18:51:35 +0900

    add purchase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from "react";
 import ReactGA from "react-ga4";
+import { v4 as uuid } from "uuid";
 
 const Home = () => {
   useEffect(() => {
@@ -9,7 +10,35 @@ const Home = () => {
     console.log("initialize");
   }, []);
 
-  return <div>Home</div>;
+  const purchase = () => {
+    const transactionId = uuid();
+
+    ReactGA.gtag("event", "purchase", {
+      transaction_id: transactionId,
+      value: 1.7,
+      currency: "USD",
+      is_first_purchase: true,
+      items: [
+        {
+          item_id: "E-GateKey",
+          item_name: "E - Gate Key",
+          affiliation: "Solo Leveling Unlimited",
+          price: 1.7,
+          quantity: 1,
+        },
+      ],
+    });
+
+    console.log("구매완료");
+  };
+
+  return (
+    <div>
+      <button type="button" onCanPlay={purchase}>
+        Purchase
+      </button>
+    </div>
+  );
 };
 
 export default Home;
